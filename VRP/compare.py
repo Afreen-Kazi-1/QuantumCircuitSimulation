7feecafd7f62afe256e70398c29edbe1262eb312
Yashvi Mehta <ydmehta3115@gmail.com>
2025-11-15 22:11:15 +0530

    Modified Vehicle Routing Problem

--- VRP/compare.py
+++ VRP/compare.py
@@ -1,5 +1,4 @@
 import time
-from classical_solver import solve_vrp_classical
 from qaoa_solver import solve_qaoa
 from pqaoa_solver import solve_pqaoa
 from vrp_qubo import build_vrp_qubo
@@ -14,18 +13,12 @@ def run():
     D = create_random_vrp(6)       # VRP instance
     Q = build_vrp_qubo(D, num_vehicles=2)
 
-    print("====== Classical Solver ======")
-    t0 = time.time()
-    c = solve_vrp_classical(D, num_vehicles=2)
-    # print("Classical:", c)
-    print("   Classical time:", time.time() - t0, "seconds\n")
-
     print("====== Standard QAOA ======")
     q = solve_qaoa(Q, p=1)
     print("QAOA:", q, "\n")
 
     print("====== Parallel QAOA (pQAOA) ======")
-    pq = solve_pqaoa(Q, r=4, p=1)
+    pq = solve_pqaoa(Q, r=2, p=1)
     print("pQAOA:", pq, "\n")
 
 if __name__ == "__main__":
